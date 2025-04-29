9a8767f98a352026954b875e932f9f730bb75afb
xbotter <xbotter@users.noreply.github.com>
2025-04-29 20:16:37 +0800

    Lucideicons fonts (#6)
    * Refactor project references in LucideIcons.MAUI.csproj
    
    - Removed direct compile references for LucideIconGlyph files and font resources.
    - Added a project reference to LucideIcons.Fonts.csproj for better modularity and maintainability.
    
    * fix: font resource path in LucideIconsMauiExtensions to ensure correct font registration

---
+++ src/LucideIcons.Fonts/LucideIconGlyph.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LucideIcons;
+
+public partial class LucideIconGlyph
+{
+    private static readonly Dictionary<string, LucideIconGlyph> _glyphs = new Dictionary<string, LucideIconGlyph>();
+    
+    public static implicit operator LucideIconGlyph(string glyph)
+    {
+        if (string.IsNullOrEmpty(glyph))
+            throw new ArgumentNullException(nameof(glyph), "Glyph name cannot be null or empty.");
+        
+        if(_glyphs.Count == 0)
+        {
+            // 预先加载所有的 glyphs
+            var fields = typeof(LucideIconGlyph).GetFields(System.Reflection.BindingFlags.Static | System.Reflection.BindingFlags.Public);
+            foreach (var field in fields)
+            {
+                if (field.GetValue(null) is LucideIconGlyph glyphObj)
+                {
+                    _glyphs[glyphObj.S_CODE] = glyphObj;
+                }
+            }
+        }
+
+        if (_glyphs.TryGetValue(glyph, out var iconGlyph))
+        {
+            return iconGlyph;
+        }
+        else
+        {
+            throw new KeyNotFoundException($"Glyph '{glyph}' not found.");
+        }
+       
+
+    }
+
+    public string S_CODE { get; }
+    public LucideIconGlyph(string code)
+    {
+        S_CODE = code;
+    }
+}
